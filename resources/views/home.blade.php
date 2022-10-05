83af73d5a96960092572d0f86e239a4a68d8104b
Renan Hagiwara <renan@recoha.com>
2022-10-05 20:07:56 -0300

    Aula 02 - Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="bootstrap.css" rel="stylesheet">
-    <script src="bootstrap.js"></script>
-  </head>
-  <body>
-    <h1>Olá {{ $nome ?? 'Sem nome' }} {{ $sobrenome }}!</h1>
-    
-    @if ($numero > 1)
-        <p>É maior</p>
-    @else
-        <p>Menor</p>
-    @endif
-  </body>
-</html>
+@extends('layout')
+
+@section('titulo', 'Home')
+@section('nome', $nome)
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>View Padrão</title>
+    <link href="bootstrap.css" rel="stylesheet">
+    <script src="bootstrap.js"></script>
+  </head>
+  <body>
+    <h1>@yield('titulo')</h1>
+
+    <p>@yield('nome')</p>
+  </body>
+</html>
